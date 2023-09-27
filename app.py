7d0b64c65cdf1d73a812fb4fe04d441f679c152a
Matheus Migliolo Coelho <132965963+Drmcoelho@users.noreply.github.com>
2023-09-27 00:46:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import openai
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
+# Initialize OpenAI API
+openai.api_key = 'your-openai-api-key'
+
+@app.route('/', methods=['GET'])
+def index():
     return 'Hello, World!'
 
+@app.route('/generate', methods=['POST'])
+def generate_text():
+    prompt = request.json.get('prompt', '')
+    try:
+        response = openai.Completion.create(
+            engine='text-davinci-002',
+            prompt=prompt,
+            max_tokens=100
+        )
+        text = response.choices[0].text.strip()
+        return jsonify({'generated_text': text})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)
